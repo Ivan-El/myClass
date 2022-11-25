7d5330f8ae4f8de871281cd67ecfbae73ea3dfb1
Иван <lostnemo@rambler.ru>
2022-11-25 15:47:30 +0600

    Добавляет переключение режима отображения списка

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const themeList = document.querySelector(".theme-list");
-const buttons = themeList.querySelectorAll(".theme-button");
+const themeButtons = themeList.querySelectorAll(".theme-button");
+const viewList = document.querySelector(".card-view-buttons");
+const viewButtons = viewList.querySelectorAll(".card-view-button");
+const cardsList = document.querySelector(".cards");
 const root = document.documentElement;
 
 const themeListClickHandler = (evt) => {
@@ -9,11 +12,30 @@ const themeListClickHandler = (evt) => {
     return;
   }
 
-  buttons.forEach((btn) => {
+  themeButtons.forEach((btn) => {
     btn.classList.toggle("active", btn === button);
   });
 
   root.dataset.themeName = button.dataset.themeSet;
 };
 
+const viewListClickHandler = (evt) => {
+    const button = evt.target.closest("button");
+
+  if (!button) {
+    return;
+  }
+
+  viewButtons.forEach((btn) => {
+    btn.classList.toggle("active", btn === button);
+  });
+
+  cardsList.className = "cards";
+  cardsList.classList.add(`${button.dataset.view}`)
+
+
+
+}
+
 themeList.addEventListener("click", themeListClickHandler);
+viewList.addEventListener("click", viewListClickHandler)

ab004ec9cf172dc1bf332392c2069d7af74af215
Иван <lostnemo@rambler.ru>
2022-11-25 15:57:30 +0600

    Выделяет переключение активной кнопки в отдельную функцию

--- js/script.js
+++ js/script.js
@@ -5,37 +5,42 @@ const viewButtons = viewList.querySelectorAll(".card-view-button");
 const cardsList = document.querySelector(".cards");
 const root = document.documentElement;
 
-const themeListClickHandler = (evt) => {
+const toggleActive = (evt, list) => {
   const button = evt.target.closest("button");
 
   if (!button) {
-    return;
+    return null;
   }
 
-  themeButtons.forEach((btn) => {
+  list.forEach((btn) => {
     btn.classList.toggle("active", btn === button);
   });
 
-  root.dataset.themeName = button.dataset.themeSet;
+  return button;
+};
+
+const themeListClickHandler = (evt) => {
+  let button;
+
+  if (toggleActive(evt, themeButtons)) {
+    button = toggleActive(evt, themeButtons);
+    root.dataset.themeName = button.dataset.themeSet;
+  } else {
+    return;
+  }
 };
 
 const viewListClickHandler = (evt) => {
-    const button = evt.target.closest("button");
+  let button;
 
-  if (!button) {
+  if (toggleActive(evt, viewButtons)) {
+    button = toggleActive(evt, viewButtons);
+    cardsList.className = "cards";
+    cardsList.classList.add(`${button.dataset.view}`);
+  } else {
     return;
   }
-
-  viewButtons.forEach((btn) => {
-    btn.classList.toggle("active", btn === button);
-  });
-
-  cardsList.className = "cards";
-  cardsList.classList.add(`${button.dataset.view}`)
-
-
-
-}
+};
 
 themeList.addEventListener("click", themeListClickHandler);
-viewList.addEventListener("click", viewListClickHandler)
+viewList.addEventListener("click", viewListClickHandler);

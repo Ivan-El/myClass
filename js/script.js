422370fdfd7857410835ab45a7372d2b3dd3a315
Иван <lostnemo@rambler.ru>
2022-11-25 14:58:39 +0600

    Добавляет переключение активного класса у кнопок темы

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-/* JavaScript код */
+const themeList = document.querySelector(".theme-list");
+const buttons = themeList.querySelectorAll(".theme-button");
+
+const themeListClickHandler = (evt) => {
+  const button = evt.target.closest("button");
+
+  if (!button) {
+    return;
+  }
+
+  buttons.forEach((btn) => {
+    console.log(btn);
+    btn.classList.toggle("active", btn === button);
+  });
+};
+
+themeList.addEventListener("click", themeListClickHandler);
